47335a919736cd9f4e11acd584f576dab0c53ca7
Jannes Höke <mail@jhoeke.de>
2020-03-28 12:00:34 +0100

    📝 Update README and add scraper template

---
+++ scraper_template.py
@@ -0,0 +1,47 @@
+import re
+from datetime import datetime, time
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+import dateparser
+import pytz
+
+from data.inhabitants import inhabitants
+from data.studios import studios
+from utils.slackbot import send_slack_message
+from utils.storage import upload_dataframe
+
+url = 'https://example.com/'
+
+def get_data():
+    # Download website
+    response = requests.get(url)
+    assert bool(response), 'Laden der Beispiel-Seite fehlgeschlagen'
+
+    # Parse into data frame
+    df = pd.DataFrame()
+
+    return df, response
+
+
+def clear_data():
+    df, response = get_data()
+
+    # Clean up data here
+
+    return df
+
+
+def write_data_example():
+    df = clear_data()
+    filename = 'example.csv'
+
+    upload_dataframe(df, filename)
+
+
+# If the file is executed directly, print cleaned data
+if __name__ == '__main__':
+    df = clear_data()
+    # print(df)
+    print(df.to_csv(index=False))
